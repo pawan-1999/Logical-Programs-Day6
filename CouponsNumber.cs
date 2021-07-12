a70bd23b58a94ff842d44868b4caa8a2afdf1609
Pawan gupta <75203671+pawan-1999@users.noreply.github.com>
2021-07-12 23:20:48 +0530

    Added - Coupon Generation Program

--- CouponsNumber.cs
+++ CouponsNumber.cs
@@ -6,14 +6,13 @@ namespace LogicalProgramDay6
 {
     class PS_5
     {
-        public int i = 0;
         public void couponsNumbers()
         {
             Console.WriteLine("Enter the number of distinct coupons you want to generate :");
             int N = Convert.ToInt32(Console.ReadLine());
             Random random = new Random();
             int[] coupomCollection = new int[N];
-            for (i = 0; i <= N-1;)
+            for (int i = 0; i <= N-1;)
             {
                 int coupon = random.Next(1001, 2001);
                 if ((coupon.CompareTo(coupomCollection[i])) == 0)
